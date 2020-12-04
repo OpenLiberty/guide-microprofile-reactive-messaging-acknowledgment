08600e4652a229811f422fd2a536eafe8c929a9a
Gilbert Kwan <gkwan@ca.ibm.com>
2020-12-04 17:36:32 -0500

    Update buildImages.bat

--- finish/scripts/buildImages.bat
+++ finish/scripts/buildImages.bat
@@ -3,7 +3,7 @@
 
 start /b docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
 start /b docker pull bitnami/zookeeper:3
-start /b docker pull bitnami/kaf
+start /b docker pull bitnami/kafka:2
 
 start /b docker build -q -t system:1.0-SNAPSHOT system\.
 start /b docker build -q -t inventory:1.0-SNAPSHOT inventory\.
