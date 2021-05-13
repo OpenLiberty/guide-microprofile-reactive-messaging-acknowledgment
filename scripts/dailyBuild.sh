941b9ac0199d6df13c6f387aaa7fa605db56f5b8
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2021-05-13 10:03:23 -0400

    shellcheck linting fix

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-while getopts t:d:b:u: flag;
-do
+while getopts t:d:b:u: flag; do
     case "${flag}" in
-        t) DATE="${OPTARG}";;
-        d) DRIVER="${OPTARG}";;
-        b) BUILD="${OPTARG}";;
-        u) DOCKER_USERNAME="${OPTARG}";;
-        *) echo "Invalid option";;
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
+    *) echo "Invalid option" ;;
     esac
 done
 
@@ -18,6 +17,6 @@ cat inventory/pom.xml system/pom.xml
 sed -i "s;FROM openliberty/open-liberty:full-java8-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile
 cat inventory/Dockerfile system/Dockerfile
 
-docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+docker pull "$DOCKER_USERNAME""/olguides:""$BUILD"
 
 ../scripts/testApp.sh
