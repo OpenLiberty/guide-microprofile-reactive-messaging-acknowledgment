fba10077aa3f3653d5ba8bb2599415ee286bbee3
Gilbert Kwan <gkwan@ca.ibm.com>
2023-02-01 15:16:05 -0500

    Update InventoryReadinessCheck.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
@@ -63,8 +63,7 @@ public class InventoryReadinessCheck implements HealthCheck {
 
     private boolean checkIfBarConsumerGroupRegistered(AdminClient adminClient) {
         ListConsumerGroupsResult groupsResult = adminClient.listConsumerGroups();
-        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture =
-        groupsResult.valid();
+        KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture = groupsResult.valid();
         try {
             Collection<ConsumerGroupListing> consumerGroups =
             consumerGroupsFuture.get();
