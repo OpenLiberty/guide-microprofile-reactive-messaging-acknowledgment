cab233feeca59b1a59897c322e33b3180aeeb539
Gilbert Kwan <gkwan@ca.ibm.com>
2023-02-01 15:17:02 -0500

    Update InventoryReadinessCheck.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/health/InventoryReadinessCheck.java
@@ -65,8 +65,7 @@ public class InventoryReadinessCheck implements HealthCheck {
         ListConsumerGroupsResult groupsResult = adminClient.listConsumerGroups();
         KafkaFuture<Collection<ConsumerGroupListing>> consumerGroupsFuture = groupsResult.valid();
         try {
-            Collection<ConsumerGroupListing> consumerGroups =
-            consumerGroupsFuture.get();
+            Collection<ConsumerGroupListing> consumerGroups = consumerGroupsFuture.get();
             for (ConsumerGroupListing g : consumerGroups) {
                 logger.info("groupId: " + g.groupId());
             }
