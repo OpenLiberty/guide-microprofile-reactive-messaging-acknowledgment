180d7284a503e64e423de298eb0981121d5c0b12
Ruilin Ma <this.is.millerma@gmail.com>
2023-11-30 13:35:13 -0500

    supporting devc mode & integration test updated

---
+++ start/scripts/startKafka.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+set NETWORK=reactive-app
+
+docker network create %NETWORK%
+
+docker run -d ^
+    -e ALLOW_PLAINTEXT_LISTENER=yes ^
+    -e KAFKA_CFG_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092 ^
+    -e KAFKA_CFG_NODE_ID=0 ^
+    -e KAFKA_CFG_PROCESS_ROLES=controller,broker ^
+    -e KAFKA_CFG_LISTENERS=PLAINTEXT://:9092,CONTROLLER://:9093 ^
+    -e KAFKA_CFG_CONTROLLER_QUORUM_VOTERS=0@kafka:9093 ^
+    -e KAFKA_CFG_CONTROLLER_LISTENER_NAMES=CONTROLLER ^
+    -p 9092:9092 ^
+    --hostname=kafka ^
+    --network=%NETWORK% ^
+    --name=kafka ^
+    --rm ^
+    bitnami/kafka:latest
